03f0834c491048f2da2c736ec7970fe3d5913411
Carlos Alognon <carlosalognon08@gmail.com>
2022-11-22 12:54:24 +0100

    change space

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ async function getData(url){
 btn.addEventListener('click', (e)=>{
     e.preventDefault()
     if(search.value.length > 0){
-        getData(`https://api.github.com/users/${search.value}`).then((data)=>{
-            
+        getData(`https://api.github.com/users/${search.value}`).then((data)=>{            
             let card = document.createElement('div')
             card.classList.add('card', 'shadow', 'rounded');
             let cardBody = document.createElement('div');
@@ -25,31 +24,23 @@ btn.addEventListener('click', (e)=>{
             } else {
                 let img = document.createElement('img');
                 img.classList.add('card-img-top', "w-100");
-                img.src = data.avatar_url
-    
+                img.src = data.avatar_url    
                 card.appendChild(img)
                 let username = document.createElement('p');
                 username.classList.add('text-muted');
                 username.innerText = data.login + " (" + data.location + ")"
-                name.innerText = data.name
-    
+                name.innerText = data.name    
                 let link = document.createElement('a');
                 link.classList.add('btn', 'btn-primary', 'w-100', 'mt-3')
                 link.href = data.html_url
                 link.setAttribute('target', '_blank')
-                link.innerText = "Voir le profil"
-    
+                link.innerText = "Voir le profil"    
                 cardBody.appendChild(username);
                 cardBody.appendChild(name)
                 cardBody.appendChild(link)
             }
-
-           
-
-
             card.appendChild(cardBody)
             results.appendChild(card)
-
             search.value = ""
         })
     } else {
