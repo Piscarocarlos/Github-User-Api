e2e028fa8d95b44ae59dd44d92e48c0826dc91a9
Carlos Alognon <carlosalognon08@gmail.com>
2022-11-22 11:56:26 +0100

    Mettre en place le fetch

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // Code here
+let btn = document.querySelector('button');
+let search = document.querySelector('input');
+
+async function getData(url){
+    let response = await fetch(url);
+    return response.json();
+}
+
+btn.addEventListener('click', (e)=>{
+    e.preventDefault()
+    if(search.value.length > 0){
+        getData(`https://api.github.com/users/${search.value}`).then((data)=>{
+            console.log(data);
+        })
+    } else {
+        alert('Vous devez entrer un utilisateur')
+    }
+})
